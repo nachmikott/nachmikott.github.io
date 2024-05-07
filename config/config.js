bf48f0633eebb5d2c38451814d4a156a8e25569a
Nachmi Kott <nachmikott@gmail.com>
2024-05-07 16:56:01 -0700

    Updates

--- config/config.js
+++ config/config.js
@@ -4,7 +4,7 @@ import { faAppStore, faGithub, faGooglePlay } from '@fortawesome/free-brands-svg
 import { } from '@fortawesome/free-solid-svg-icons';
 
 export const navigation = {
-  name: "Hashir",
+  name: "Nachmi",
   links: [
     {
       title: "About",
@@ -24,13 +24,13 @@ export const navigation = {
     },
     {
       title: "Blog",
-      link: "https://medium.com/@hashirshoaeb",
+      link: "TBD",
     }
   ],
 }
 export const intro = {
-  title: "Hey, I'm Hashir",
-  description: "A Computer Engineer creating mobile apps and static websites.",
+  title: "Hey, I'm Nachmi",
+  description: "A software engineer building and developing meaningful experiences for customers, clients, myself and colleagues.",
   image: profile.src,
   buttons: [
     {
@@ -40,7 +40,7 @@ export const intro = {
     },
     {
       title: "Resume",
-      link: "https://docs.google.com/document/d/13_PWdhThMr6roxb-UFiJj4YAFOj8e_bv3Vx9UHQdyBQ/edit?usp=sharing",
+      link: "https://docs.google.com/document/d/1Z44Xt87s5C_Dr6UPxCSSauhs7_Ru3xbEFZiBl-KY4Bs/edit?usp=sharing",
       isPrimary: false,
     },
   ],
@@ -49,9 +49,9 @@ export const intro = {
 export const about = {
   title: "Who I am",
   description: [
-    "I graduated from National University of Sciences and Technology (NUST) in 2020 with a degree in Computer Engineering. I kick started my professional career as a mobile application developer working remotely for a UK based startup — Lessgo.",
-    "The tech industry is ever-evolving and I love being able to grow with it, while continually solidifying the fundamentals. I opensource my code from a place of empathy - for future developers, teammates, users, and with accessibility in mind. I also love joining communities, helping and mentoring new developers, and supporting organizations to promote diversity in tech. I’m sharing my programming journey on instagram, helping people who are just getting into the space of programming.",
-    "When I’m not programming, I focus on my hobbies which are: diary writing, doodling, meeting people and growing my network.",
+    "I graduated from The University of Maryland in 2017 with a Bachelor of Computer Science and Minor in Technology Entrepreneurship. I have 6+ years of experience working on web applications, backend APIs, databases and distributed system infrastructure at both enterprise and individual client scale.",
+    "Currently, I'm working at Curiosity Media (IXL Learning) as a Senior Software Engineer, building interactive and impactful experiences for those learning Spanish or English as a second language.",
+    "Outside of engineering, I'm often skiing, tutoring students in software engineering, traveling, spending time with family and friends, working on a woodworking project, or playing the keyboard.",
   ],
 }
 
@@ -59,13 +59,23 @@ export const work = {
   title: "What I do",
   cards: [
     {
-      title: "Mobile App Development",
-      description: "I create pixel perfect iOS and Andriod apps using Flutter.",
+      title: "Project Managament",
+      description: "I turn ideas into deliverables. I have experience turning business initiatives into technical solutions using agile methodologies and engineering best practices.",
       icons: null,
     },
     {
       title: "Web Development",
-      description: "I create responsive static websites using Reactjs.",
+      description: "I create responsive and impactful websites. I am customer-experience centric, striving to build delightful experiences for users.",
+      icons: null,
+    },
+    {
+      title: "System Design",
+      description: "I can design systems beyond just websites. This includes end-to-end testing, CICD and 3rd party integrations at scale.",
+      icons: null,
+    },
+    {
+      title: "Mentoring and Tutoring",
+      description: "I am an experienced mentor and tutor in software engineering. I have helped numerous mentors and tutees achieve their goals and aspirations.",
       icons: null,
     }
   ],
@@ -75,40 +85,22 @@ export const projects = {
   title: "Projects",
   cards: [
     {
-      title: "StarBook",
-      description: "A digital diary and mood tracking app that helps you in keeping track of your mood and productivity throughout the month/year.",
-      icons: [
-        {
-          icon: faAppStore,
-          link: "https://apps.apple.com/us/app/starbook-journal-ai-diary/id1552418289",
-        },
-        {
-          icon: faGithub,
-          link: "https://github.com/hashirshoaeb/star_book",
-        },
-      ]
-    },
-    {
-      title: "QuranTalk",
-      description: "An emotional well being and mental health app. The app helps people to navigate their emotions in the light of the Quran.",
-      icons: [
-        {
-          icon: faAppStore,
-          link: "https://apps.apple.com/us/app/qurantalk/id1563425149",
-        },
-        {
-          icon: faGooglePlay,
-          link: "https://play.google.com/store/apps/details?id=com.ayahemotion.quran_talk",
-        },
-      ]
-    },
-    {
-      title: "Portfolio",
-      description: "A portfolio website template that helps you showcase your work, projects and skills as a software developer or freelancer.",
+      title: "WIP - How I Built This",
+      description: "Embed walkthroughs, documentation, tutorials and other learning resources into your application. - More details coming soon.",
       icons: [
         {
           icon: faGithub,
-          link: "https://github.com/hashirshoaeb/portfolio",
+          link: "",
+        }
+      ]
+    },
+    {
+      title: "WIP - Ancestry Storyteller",
+      description: "AI Project in the Geneaology space - more details coming soon.",
+      icons: [
+        {
+          icon: faGithub,
+          link: "",
         },
       ]
     },
@@ -117,16 +109,16 @@ export const projects = {
 
 export const contact = {
   title: "Get in touch",
-  description: "Coffee Chat! Please do not hesitate to schedule a meeting. Alternatively, feel free to reach out directly by email at hashirshoaeb@gmail.com.",
+  description: "Coffee Chat! Please do not hesitate to schedule a meeting. Alternatively, feel free to reach out directly by email at nachmikott@gmail.com.",
   buttons: [
     {
       title: "Email Me",
-      link: "mailto:hashirshoaeb@gmail.com",
+      link: "mailto:nachmikott@gmail.com",
       isPrimary: true,
     },
     {
       title: "Schedule Meeting",
-      link: "https://topmate.io/hashirshoaeb",
+      link: "https://calendly.com/nachmikott/30min",
       isPrimary: false,
     },
   ]
@@ -135,35 +127,23 @@ export const contact = {
 // SEARCH ENGINE 
 export const SEO = {
   // 50 - 60 char  
-  title: "Hashir Shoaib | Computer Engineer | Flutter | Reactjs developer",
-  description: "I create mobile apps and static websites. I graduated from National University of Sciences and Technology (NUST) in 2020 with a degree in Computer Engineering.",
+  title: "Nachmi Kott | FullsStack Software Engineer",
+  description: "Software Engineer | Freelance",
   image: profile.src,
 }
 
 export const links = {
   image: profile.src,
-  title: "@hashirshoaeb",
-  description: "Computer Engineer | Flutter | Reactjs Developer",
+  title: "@nachmikott",
+  description: "Fullstack Software Engineer",
   cards: [
     {
-      title: "My website",
-      link: "https://hashirshoaeb.com/",
-    },
-    {
-      title: "QuranTalk App",
-      link: "https://www.qurantalk.app/",
-    },
-    {
-      title: "StarBook App",
-      link: "https://starbook.dev/",
-    },
-    {
       title: "My GitHub",
-      link: "https://github.com/hashirshoaeb/",
+      link: "https://github.com/nachmikott/",
     },
     {
       title: "My LinkedIn",
-      link: "https://www.linkedin.com/in/hashirshoaeb/",
+      link: "https://www.linkedin.com/in/nachmikott/",
     },
   ]
 }
