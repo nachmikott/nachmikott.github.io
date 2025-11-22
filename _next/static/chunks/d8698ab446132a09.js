693d3743375f8d1f89dbba64c203ef07ece75b6b
nachmikott <nachmikott@gmail.com>
2025-11-22 05:25:07 +0000

    Deploying to gh-pages from @ nachmikott/nachmikott.github.io@68bfe6e0dae0b3f7a78afd31cbac24597bb0ae37 🚀

--- _next/static/chunks/d8698ab446132a09.js
+++ _next/static/chunks/d8698ab446132a09.js
@@ -3,6 +3,6 @@ __turbopack_load_page_chunks__("/_app", [
   "static/chunks/984bf2977571956c.js",
   "static/chunks/5203b6ea975a75ef.js",
   "static/chunks/5f541aa8ac016452.js",
-  "static/chunks/ab6d759a9f9de6e7.css",
-  "static/chunks/turbopack-c37d15579405be72.js"
+  "static/chunks/09cf9cdeda9102e5.css",
+  "static/chunks/turbopack-3b39b3178de1bde9.js"
 ])
